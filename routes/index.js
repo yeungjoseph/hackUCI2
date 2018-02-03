5880e2a5976dd264d9726495011df4a70659421a
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-03 13:06:20 -0800

    add post

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res, next) {
   res.sendFile('expire.txt', { root: path.join(__dirname, '../public') })
 });
 
+router.post('/', function(req, res, next) {
+  res.send('Yeah I got the request');
+  console.log(req.body.photo);
+});
+
 module.exports = router;
 
 //https://stackoverflow.com/questions/43787515/making-external-get-request-with-express
