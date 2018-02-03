b576b633eadc58d242eca2382d5f190b097d4ec8
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-03 14:51:57 -0800

    got the google api to work

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var request = require('request');
 
 
 /* GET home page. */
@@ -11,9 +12,34 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   res.send('Yeah I got the request');
-
-  console.log("GOT EM");
-  console.log(req.body);
+  var url =   req.body.url;
+  var requestData = {
+    "requests":[
+      {
+        "image":{
+          "source":{
+            "imageUri":
+              "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          }
+        },
+        "features":[
+          {
+            "type":"LOGO_DETECTION",
+            "maxResults":1
+          }
+        ]
+      }
+    ]
+  }
+  request.post({
+      url: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDs5HW2OUYCStIk67GkQ5efoT4zznsPmPw",
+      json: requestData
+  }, function(err, httpResponse, body) {
+      if (err)
+        return console.error(err)
+    console.log('Upload successful!  Server responded with:', body);
+    console.log(body.responses[0]);
+  });
 });
 
 module.exports = router;
