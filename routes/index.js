17d9b6d3266ef6019ca7fb3c7271b390ec4c66ec
Joseph Yeung <yeungjoseph10@gmail.com>
2018-02-03 10:24:21 -0800

    Able to return text file to client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve('../hackUCI2/public/expire.txt'))
+  //res.render('home');
 });
 
 module.exports = router;
+
+//https://stackoverflow.com/questions/43787515/making-external-get-request-with-express
