7ecbe9b4f8e0effb4aa34e459e5bb9985e195d32
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2018-02-04 04:09:53 -0800

    FINISHED

--- public/main2.js
+++ public/main2.js
@@ -24,10 +24,36 @@ function upload() {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function(response) {
+                console.log(typeof response);
                 console.log(response);
-            }
-        });
 
+
+
+                var myCalendar = createCalendar({
+                    options: {
+                        class: '',
+
+                    },
+                    data: {
+                        title: "Expiration: " + response.name,
+                        start: new Date(response.date),
+                        duration: 60,
+                    }
+                });
+
+                document.querySelector('.new-cal').appendChild(myCalendar);
+
+
+                var anchor = document.querySelector('.icon-google');
+                window.open(anchor.href);
+
+                // console.log(document.querySelector('.icon-google').href);
+                // console.log($('.icon-google').length);
+                // $('.icon-google').attr('id', 'test');
+                // $('#test').click();
+            }
+
+        });
         console.log("request sent");
     });
     fileReader.readAsDataURL(file);
